661a57e2ee5c665232772debfe56dc4ee8c581a3
se0863 <778613414@qq.com>
2019-03-15 15:29:52 +0800

    feat: move to to old_config.json

--- fcm_test/old_http.js
+++ fcm_test/old_http.js
@@ -1,11 +1,10 @@
 const request = require('request')
 
-const key = require('./old_key.json').key  //"AIzaSyAUxLbb1S5fy9PuH8lkG6gx8D315VWmmkw" 示例，非实际使用
+const key = require('./old_config.json').key
+const to  = require('./old_config.json').to
 const url1 = 'https://fcm.googleapis.com/fcm/send'       //新接口
 const url2 = 'https://android.googleapis.com/gcm/send'   //使用的旧接口
 const url3 = 'https://gcm-http.googleapis.com/gcm/send'  //文档的旧接口
-
-var to = 'eQ0gqZz_yLY:APA91bEP41xfpwMuxAA4UQePm60u0skUaL24AW6wa6KbnYVHAsVLxFnP327dEgLouXkeKaf_Oh-d4WbagJYD_603gpHJ6AHWjCK9NA6mW_zbuGRJl2xUd4wmhH0-HEU0SN9jpqC6n7s0'
 
 old_push(url1, key, to)
 
